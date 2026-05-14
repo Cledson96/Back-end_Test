f8d3849ca4b144bb288d1225928fad9e34af4ab5
Cledson Santos Souza <cledson96@users.noreply.github.com>
2026-05-14 01:40:24 -0300

    (feature) implement Newton-Raphson algorithm

--- server/src/common/models/square-root/newton-raphson-algorythm.class.ts
+++ server/src/common/models/square-root/newton-raphson-algorythm.class.ts
@@ -1,9 +1,9 @@
 import { SqrtAlgorithm } from "./sqrt-algorythm.a-class";
 
-// Implementation of the Newton-Raphson algorithm: x(n+1) = 0.5 * (x(n) + number/x(n))
-// 1. Start with an initial guess
-// 2. Update the guess by averaging it with the number divided by the guess
-// 3. Repeat step 2 until the guess is within the tolerance
-// 4. Return the guess
+export class NewtonRaphsonAlgorithm extends SqrtAlgorithm {
+	protected approximateGuess(): number {
+		const currentGuess = this.result === 0 ? this.number / 2 : this.result;
 
-export class NewtonRaphsonAlgorithm extends SqrtAlgorithm {}
+		return 0.5 * (currentGuess + this.number / currentGuess);
+	}
+}
